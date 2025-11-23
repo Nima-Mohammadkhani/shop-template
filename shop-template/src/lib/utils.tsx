b25f16d5f8ad5ae032d1c59c92db10bf048ed06c
Nima <197469689+Nima-Mohammadkhani@users.noreply.github.com>
2025-11-23 22:31:42 +0330

    add toPersianNumber function

--- shop-template/src/lib/utils.tsx
+++ shop-template/src/lib/utils.tsx
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: (string | undefined | false | null)[]) {
   return twMerge(clsx(inputs));
 }
+
+export const toPersianNumber = (input: string | number): string => {
+  const persianDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+  const englishDigits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  let str = input?.toString();
+
+  for (let i = 0; i < 10; i++) {
+    str = str?.replace(new RegExp(englishDigits[i], "g"), persianDigits[i]);
+  }
+
+  return str;
+};
