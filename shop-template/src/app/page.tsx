082bc43be934e07b0928819158326c0b62bc2e8d
Nima <197469689+Nima-Mohammadkhani@users.noreply.github.com>
2025-11-23 22:32:00 +0330

    add Card component

--- shop-template/src/app/page.tsx
+++ shop-template/src/app/page.tsx
@@ -1,9 +1,47 @@
 import Slider from "@/components/slider";
+import Card from "@/components/ui/Card";
 
 const Home = () => {
+  const product = [
+    {
+      id: 1,
+      image: "/image/image/product/pic.png",
+      name: "قهوه ساز دلونگی",
+      model: "N-lITE 203 aIRlINKS",
+      star: 4.3,
+      price: 2400000,
+      off: 30,
+    },
+    {
+      id: 2,
+      image: "/image/image/product/pic2.png",
+      name: "استند چوبی آشپزخانه",
+      model: "N-lITE 203 aIRlINKS",
+      star: 4.3,
+      price: 2400000,
+      off: 30,
+    },
+    {
+      id: 3,
+      image: "/image/image/product/pic3.png",
+      name: "مبل دو نفره سپنتا",
+      model: "Sepanta-01-NDF",
+      star: 4.3,
+      price: 2400000,
+      off: 30,
+    },
+  ];
   return (
-    <section> 
+    <section className="flex flex-col gap-16">
       <Slider />
+      <div className="mx-auto w-full max-w-7xl">
+        <div className="flex justify-between w-full max-w-7xl">
+          <div className="bg-primary border-2 border-b-4 border-neutral-200 rounded-2xl shadow-xl h-96 w-2xs"></div>
+          {product.map((productItem) => (
+            <Card productItem={productItem} key={productItem.id} />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
---
+++ shop-template/src/components/ui/Card.tsx
@@ -0,0 +1,49 @@
+import { toPersianNumber } from "@/lib/utils";
+
+interface IProductItem {
+  id: number;
+  image: string;
+  name: string;
+  model: string;
+  star: number;
+  price: number;
+  off: number;
+}
+
+interface CardProps {
+  productItem: IProductItem;
+}
+
+const Card: React.FC<CardProps> = ({ productItem }) => {
+  const discount = (productItem.price * productItem.off) / 100;
+  const finalPrice = productItem.price - discount;
+  return (
+    <section className="flex flex-col justify-around p-4 bg-white border-2 border-b-4 border-neutral-200 rounded-2xl shadow-xl h-96 w-2xs">
+      <img src={productItem.image} alt="" className="h-1/2 object-cover" />
+      <div className="flex flex-col flex-1 justify-between text-sm pt-4">
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col flex-1 gap-1">
+            <h3>{productItem.name}</h3>
+            <h3>{toPersianNumber(productItem.model)}</h3>
+          </div>
+          <div className="flex items-center gap-1">
+            <h4>{toPersianNumber(productItem.star)}</h4>
+            <p>star-icon</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h4 className="self-end">
+            {toPersianNumber(productItem.price.toLocaleString())}
+          </h4>
+          <div className="flex justify-between items-center">
+            <div className="rounded-3xl bg-primary-500 text-white px-4 py-1">
+              {toPersianNumber(productItem.off)}%
+            </div>
+            <h4>{toPersianNumber(finalPrice.toLocaleString())}</h4>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Card;
