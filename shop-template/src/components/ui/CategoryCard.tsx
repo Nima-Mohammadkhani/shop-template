1f42b3eebb892bd45285f084880137a23c4cdab0
Nima <197469689+Nima-Mohammadkhani@users.noreply.github.com>
2025-11-29 14:41:53 +0330

    update and fixed bug

--- shop-template/src/components/ui/CategoryCard.tsx
+++ shop-template/src/components/ui/CategoryCard.tsx
@@ -9,7 +9,7 @@ interface CategoryCardProps {
 }
 const CategoryCard: React.FC<CategoryCardProps> = ({ categoryItem }) => {
   return (
-    <section className="flex flex-col justify-center items-center sm:gap-4 text-base bg-white border-2 border-b-4 border-neutral-200 size-48 sm:h-56 sm:w-2xs rounded-2xl shadow-xl">
+    <section className="flex flex-col justify-center items-center sm:gap-4 text-base bg-white border-2 border-b-4 border-neutral-200 size-42 sm:h-56 sm:w-2xs rounded-2xl shadow-xl">
       <img src={categoryItem.image} alt="" />
       <h3 className="text-sm sm:text-base">{categoryItem.category}</h3>
     </section>
